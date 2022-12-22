3d59a5ee075ae47b975ce8b2809989d4a486d359
iremberber <iremberber0@gmail.com>
2022-12-22 22:35:22 +0300

    js ders 1

---
+++ app.js
@@ -0,0 +1,23 @@
+document.writeln("Hello World");
+document.writeln("Goodbye World");
+
+console.log("3.44");
+console.log({name:"irem",surname:"berber"});
+
+//console.clear();
+
+let a=5;
+let b=25;
+
+console.log(a+b);
+console.log("İki sayının toplamı:",a+b);
+
+alert("dikkat dikkat");
+alert("iki sayının toplamı:"+(a+b));
+
+console.log(window);
+
+console.error("hata olustu");
+
+console.warn ("hata olabilir");
+
